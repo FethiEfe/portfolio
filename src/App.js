db93d95b1b2fb02dbfa95171862d5e8c28e93d18
Fethi E <jack@jacks-MacBook-Pro-2.local>
2019-05-30 16:46:01 -0500

    animation added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./Components/About"
 import Contact from "./Components/Contact"
 import Home from "./Components/Home"
 import Projects from "./Components/Projects"
-import Develop4Good from "./Components/Develop4Good"
+
 import "./App.css"
 
 function App() {
