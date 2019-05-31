e2f4e1f708bd2a695b9dcd03c5a8c32999ab1c7c
Fethi E <jack@jacks-MacBook-Pro-2.local>
2019-05-30 23:23:14 -0500

    hosting with personal project

--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 const express = require("express")
 const { SERVER_PORT } = process.env
 const Controller = require("./Controller")
+const path = require('path'); 
 const app = express()
 app.use(express.json())
+app.use( express.static( `${__dirname}/../build` ) );
 
 app.post(`/api/contact`, Controller.main)
 
-
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 app.listen(SERVER_PORT, () => {
     console.log(`I am listening on ${SERVER_PORT}`)
   })
