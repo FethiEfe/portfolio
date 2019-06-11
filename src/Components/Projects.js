71229a8c457255d86b8e8e15505bfe3627aefb3b
Fethi E <jack@jacks-MBP-2.satx.rr.com>
2019-06-10 23:32:24 -0500

    group project added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,6 +3,9 @@ import { Carousel, Modal, Button } from "react-bootstrap"
 import develop4good1 from "../Images/develop4good1.png"
 import develop4good2 from "../Images/develop4good2.png"
 import develop4good3 from "../Images/develop4good3.png"
+import tuneflix1 from "../Images/tuneflix1.jpg"
+import tuneflix2 from "../Images/tuneflix2.jpg"
+import tuneflix3 from "../Images/tuneflix3.jpg"
 
 class Projects extends React.Component {
   constructor() {
@@ -75,14 +78,78 @@ class Projects extends React.Component {
 
                   </ul>
                 </Modal.Body>
-                <Modal.Footer>
+                {/* <Modal.Footer>
                   <Button variant="secondary" onClick={this.handleClose}>
                     Close
                 </Button>
-                </Modal.Footer>
+                </Modal.Footer> */}
               </Modal>
               <a href="https://develop4good.com" target="_blank">Live Site</a>
               <a href="https://github.com/FethiEfe/develop4good" target="_blank">Github</a>
+            </div>
+
+
+          </div>
+
+          <div className="Projects-Card">
+            <Carousel id="Carousel">
+              <Carousel.Item >
+                <img
+                  className="d-block w-100"
+                  src={tuneflix1}
+                  alt="First slide"
+
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={tuneflix2}
+                  alt="First slide"
+
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={tuneflix3}
+                  alt="First slide"
+
+                />
+              </Carousel.Item>
+            </Carousel>
+            <p className="exp-parag">Tuneflix lets you find the soundtrack to any movie or TV series</p>
+            <div className="project-but">
+              {/* <a>More Info</a> */}
+              <a variant="primary" onClick={this.handleShow}>
+                More Info
+            </a>
+
+              <Modal show={this.state.show} onHide={this.handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>TuneFlix</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <p>React | Redux | NodeJS | Express | SASS | JEST | CYPRESS</p>
+
+                  <ul>
+                    <li>TuneFlix is a fully responsive application that lets you find the soundtrack to any movie or TV series by simply typing in the name</li>
+                    <li>Tuneflix recognizes music in microphone records and in audio files.</li>
+                    <li>Combined multiple API's (IMDB, Tunefind, Audd) to ensure a consistent user experience.</li>
+                    <li>Implemented Redux for state management throughout the application.</li>
+                    <li>Integrated Redux to hold and manage state</li>
+                    <li>Wrote unit and end-to-end tests to run quality assurance on the entire application.</li>
+
+                  </ul>
+                </Modal.Body>
+                {/* <Modal.Footer>
+                  <Button variant="secondary" onClick={this.handleClose}>
+                    Close
+                </Button>
+                </Modal.Footer> */}
+              </Modal>
+              <a href="https://tuneflix.fun" target="_blank">Live Site</a>
+              <a href="https://github.com/tuneflix/tuneflix" target="_blank">Github</a>
             </div>
 
 
